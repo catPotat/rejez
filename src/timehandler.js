fa2488fc5b48cf3f7fe14b328f5efdc6667ac978
mat <mathiaslsn2@gmail.com>
2020-02-15 23:55:48 -0500

    Add Hypno KK

--- src/timehandler.js
+++ src/timehandler.js
@@ -5,6 +5,9 @@ const badgeColors = {"12am":"#000000","1am":"#21211d","2am":"#28281f","3am":"#35
 const kkDefaultSongs = ["K.K. Casbah","K.K. Chorale Aircheck","K.K. Chorale","K.K. Condor Aircheck","K.K. Condor","K.K. Country Aircheck","K.K. Country","K.K. Cruisin' Aircheck","K.K. Cruisin'","K.K. D & B Aircheck","K.K. D & B","K.K. Dirge Aircheck","K.K. Dirge","K.K. Dixie Aircheck","K.K. Dixie","K.K. Etude Aircheck","K.K. Etude","K.K. Faire Aircheck","K.K. Faire","K.K. Folk Aircheck","K.K. Folk","K.K. Fusion Aircheck","K.K. Fusion","K.K. Gumbo Aircheck","K.K. Gumbo","K.K. Jazz Aircheck","K.K. Jazz","K.K. Lament Aircheck","K.K. Lament","K.K. Love Song Aircheck","K.K. Love Song","K.K. Lullaby Aircheck","K.K. Lullaby","K.K. Mambo Aircheck","K.K. Mambo","K.K. Marathon ~ Aircheck","K.K. Marathon","K.K. March Aircheck","K.K. March","K.K. Metal Aircheck","K.K. Metal","K.K. Parade Aircheck","K.K. Parade","K.K. Ragtime Aircheck","K.K. Ragtime","K.K. Rally Aircheck","K.K. Rally","K.K. Reggae Aircheck","K.K. Reggae","K.K. Rock Aircheck","K.K. Rock","K.K. Rockabilly Aircheck","K.K. Rockabilly","K.K. Safari Aircheck","K.K. Safari","K.K. Salsa Aircheck","K.K. Salsa","K.K. Samba Aircheck","K.K. Samba","K.K. Ska Aircheck","K.K. Ska","K.K. Song Aircheck","K.K. Song","K.K. Soul Aircheck","K.K. Soul","K.K. Steppe Aircheck","K.K. Steppe","K.K. Swing Aircheck","K.K. Swing","K.K. Tango Aircheck","K.K. Tango","K.K. Technopop Aircheck","K.K. Technopop","K.K. Waltz Aircheck","K.K. Waltz","K.K. Western Aircheck","K.K. Western","King K.K. Aircheck","King K.K.","Lucky K.K. Aircheck","Lucky K.K.","Marine Song 2001 Aircheck","Marine Song 2001","Mountain Song ~ Aircheck","Mountain Song","Mr. K.K. Aircheck","Mr. K.K.","My Place Aircheck","My Place","Neapolitan Aircheck","Neapolitan","Only Me Aircheck","Only Me","Pondering Aircheck","Pondering","Rockin' K.K. Aircheck","Rockin' K.K.","Senor K.K. Aircheck","Senor K.K.","Sorry, That Just Isn't My Bag 1","Sorry, That Just Isn't My Bag 2","Sorry, That Just Isn't My Bag 3","Soulful K.K. Aircheck","Soulful K.K.","Steep Hill Aircheck","Steep Hill","Surfin' K.K. Aircheck","Surfin' K.K.","The K. Funk Aircheck","The K. Funk","To the Edge Aircheck","To the Edge","Two Days Ago Aircheck","Two Days Ago","Agent K.K. Aircheck","Agent K.K.","Aloha K.K. Aircheck","Aloha K.K.","Cafe K.K. Aircheck","Cafe K.K.","Comrade K.K. Aircheck","Comrade K.K.","DJ K.K. Aircheck","DJ K.K.","Forest Life Aircheck","Forest Life","Go K.K. Rider! Aircheck","Go K.K. Rider!","I Love You Aircheck","I Love You","Imperial K.K. Aircheck","Imperial K.K.","Just Pulling Your Leg 1","Just Pulling Your Leg 2","Just Pulling Your Leg 3","K.K. Aria Aircheck","K.K. Aria","K.K. Ballad Aircheck","K.K. Ballad","K.K. Blues Aircheck","K.K. Blues","K.K. Bossa Aircheck","K.K. Bossa","K.K. Calypso Aircheck","K.K. Calypso","K.K. Casbah Aircheck"]
 let kkSongs = kkDefaultSongs
 
+// Cloudfront URL
+const baseURL = 'https://d17orwheorv96d.cloudfront.net'
+
 // GM Hour Array
 const gmDefaultHours = ['12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm']
 let gmHours = gmDefaultHours
@@ -50,7 +53,7 @@ function playRandomKK (check = false) {
 function playRain () {
   if (rainSound) rainSound.unload()
   rainSound = new Howl({
-    src: [`https://d17orwheorv96d.cloudfront.net/rain/rain.ogg`],
+    src: [`${baseURL}/rain/rain.ogg`],
     loop: true,
     volume: 0
   })
@@ -102,7 +105,7 @@ function playSound (name, hour, kk) {
   let thisGame = game
   if (kk) thisGame = 'kk-slider'
   sound = new Howl({
-    src: [`https://d17orwheorv96d.cloudfront.net/${thisGame}/${!kk && grandfatherMode ? 'full/' : ''}${name}.ogg`],
+    src: [`${baseURL}/${thisGame}/${!kk && grandfatherMode ? 'full/' : ''}${name}.ogg`],
     loop: !grandfatherMode,
     volume: 0,
     onend: () => {
